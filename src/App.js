e0c0181fa2a6661c39b25466f2dad27abeb199d0
Razaq Rzn <razaqrzn@gmail.com>
2024-07-29 18:20:57 +0530

    completed header sectio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/screens/Home";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
--- src/components/includes/Header.js
+++ src/components/includes/Header.js
@@ -1,7 +1,154 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <HeaderContainer>
+        <HeaderWrapper>
+          <LogoContainer>
+            <Image
+              src={require("../../components/assets/Logo.svg").default}
+              alt="Logo image"
+            />
+          </LogoContainer>
+          <Navbar>
+            <Menu>
+              <MenuItem>
+                <MenuLink href="#">Shop</MenuLink>
+              </MenuItem>
+              <MenuItem>
+                <MenuLink href="#">Men</MenuLink>
+              </MenuItem>
+              <MenuItem>
+                <MenuLink href="#">Women</MenuLink>
+              </MenuItem>
+              <MenuItem>
+                <MenuLink href="#">Combos</MenuLink>
+              </MenuItem>
+              <MenuItem>
+                <MenuLink href="#">Joggers</MenuLink>
+              </MenuItem>
+            </Menu>
+          </Navbar>
+          <SearchBar>
+            <Button>
+              <SearchIcon
+                src={require("../../components/assets/search.svg").default}
+              />
+            </Button>
+            <Input placeholder="Search" />
+          </SearchBar>
+          <UserMenu>
+            <UserButton>
+              <UserIconContainer>
+                <UserIcon
+                  src={require("../../components/assets/wishlist.svg").default}
+                  alt="User icon"
+                />
+              </UserIconContainer>
+            </UserButton>
+            <UserButton>
+              <UserIconContainer>
+                <UserIcon
+                  src={require("../../components/assets/account.svg").default}
+                  alt="User icon"
+                />
+              </UserIconContainer>
+            </UserButton>
+            <UserButton>
+              <UserIconContainer>
+                <UserIcon
+                  src={require("../../components/assets/cart.svg").default}
+                  alt="User icon"
+                />
+              </UserIconContainer>
+            </UserButton>
+          </UserMenu>
+        </HeaderWrapper>
+      </HeaderContainer>
+    </>
+  );
 }
+const HeaderContainer = styled.header`
+  display: flex;
+  height: 108px;
+  border-bottom: 1px solid #bebcbd;
+`;
+
+const HeaderWrapper = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  max-width: 1280px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LogoContainer = styled.div``;
+
+const Image = styled.img`
+  width: 100%;
+  display: block;
+`;
+
+const Navbar = styled.nav``;
+const Menu = styled.ul`
+  display: flex;
+  list-style-type: none;
+  gap: 40px;
+`;
+const MenuItem = styled.li``;
+const MenuLink = styled(Link)`
+  text-decoration: none;
+  color: #807d7e;
+  &:focus {
+    color: #3c4242;
+  }
+`;
+const SearchBar = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 20px;
+  background-color: #f6f6f6;
+  border-radius: 8px;
+`;
+const Button = styled.button`
+  background-color: transparent;
+  border: none;
+  margin-right: 12px;
+`;
+
+const SearchIcon = styled.img`
+  width: 100%;
+  display: block;
+`;
+
+const Input = styled.input`
+  all: unset;
+  box-sizing: border-box;
+  width: 100%;
+`;
+const UserMenu = styled.div`
+  display: flex;
+  gap: 12px;
+  justify-content: center;
+  align-items: center;
+`;
+const UserButton = styled(Link)``;
+const UserIconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 44px;
+  height: 44px;
+  paddings: 12px;
+  border-radius: 8px;
+  background-color: #f6f6f6;
+`;
+
+const UserIcon = styled.img``;
 
 export default Header;
