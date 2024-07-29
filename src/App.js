e14e8560d55e88d806f960443b8bba10c8051627
Razaq Rzn <razaqrzn@gmail.com>
2024-07-29 12:09:34 +0530

    project created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
